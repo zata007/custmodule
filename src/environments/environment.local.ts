2895cb9badab791a30787aaa63b5920e0de641dc
Avinash Maurya <avinash.gic@gmail.com>
2020-01-06 21:58:35 +0530

    #Added build environment for local

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  API_Endpoint: 'https://d.z07.xyz',
+  OA_API_Endpoint: 'https://d.z07.xyz/oa',
+  USER_API_Endpoint: 'https://d.z07.xyz/user',
+  SOCKET_API_Endpoint: 'https://d.z07.xyz',
+  referralURL: 'https://d.z07.xyz/#/pre-launch/main/',
+  mapApiKey: 'AIzaSyB9rjY3fL8F0OUcy1ubyFnAVl96FhbadVY',
+  pushNotifyKeys: {
+    publicKey: 'BMLM_MOg0aQQThjidH5CkW9I4VyIdjA_pRWDnMAlmQR1_jHiDU3E7SP-V7125EY22SGq-941VxEi8ahKKpl7boE',
+    privateKey: 'Zye3OquXF4C34tN6N-V237YYFaVroktHhxbj0v7t0ls',
+  },
+  sso: {
+    google: '24984653661-06sld27dqqfqme78kes840qrc06ggs33.apps.googleusercontent.com',
+    fb: '2551119675116311',
+  },
+};
