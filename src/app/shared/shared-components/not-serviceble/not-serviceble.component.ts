4693b31d4af9a8931dd848a11d4e8deb13e98f71
Avinash Maurya <avinash.gic@gmail.com>
2020-01-08 20:40:16 +0530

    #Minor refactoring

--- src/app/shared/shared-components/not-serviceble/not-serviceble.component.ts
+++ src/app/shared/shared-components/not-serviceble/not-serviceble.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, ViewEncapsulation, Inject } from "@angular/core";
-import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from "@angular/material";
+import { Component, OnInit, ViewEncapsulation, Inject } from '@angular/core';
+import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material';
 
 @Component({
-  selector: "app-not-serviceble",
-  templateUrl: "./not-serviceble.component.html",
-  styleUrls: ["./not-serviceble.component.scss"],
+  selector: 'app-not-serviceble',
+  templateUrl: './not-serviceble.component.html',
+  styleUrls: ['./not-serviceble.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class NotServicebleComponent implements OnInit {
   isSubmitRequestVisible = false;
-  location = "";
-  mailText = "";
+  location = '';
+  mailText = '';
   restaurantData = {
-    name: "",
-    contact: "",
-    address: ""
+    name: '',
+    contact: '',
+    address: ''
   };
   constructor(
     private bottomSheetRef: MatBottomSheetRef<NotServicebleComponent>,
@@ -31,6 +31,10 @@ export class NotServicebleComponent implements OnInit {
 
   closePage() {
     this.bottomSheetRef.dismiss();
+  }
+
+  proceed(){
+    this.isSubmitRequestVisible = true;
   }
 
   mailMe() {
