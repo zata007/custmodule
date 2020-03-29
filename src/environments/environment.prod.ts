c78eb3eb14f88133e1df98dc892b860b1bf2a895
Avinash Maurya <avinash.gic@gmail.com>
2020-03-29 21:43:59 +0530

    #Production environment ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  API_Endpoint: 'https://z07.xyz:8001',
+  SOCKET_API_Endpoint: 'https://s.z07.xyz',
+  referralURL: 'https://z07.xyz/#/pre-launch/main/',
+  paymentUrl: 'https://z07.xyz',
+  mapApiKey: 'AIzaSyB9rjY3fL8F0OUcy1ubyFnAVl96FhbadVY',
+  pushNotifyKeys: {
+    publicKey: 'BB0X1vjLPXA3v1trZR9KKOC3EqNT_V__Liv5jP32Ywl7snyWz61aClgeaevS3Lp9QjxGvuaWWBnAWJnr4xAzyaU',
+    privateKey: 'PuGlNnf-xd167xVMg17lJH7vq3sENZM9c8oTrT_F_14',
+  },
 };
